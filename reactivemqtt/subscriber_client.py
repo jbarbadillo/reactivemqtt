1b944d23c6b0daf680cb6b116db874053f0e871e
jbarbadillo <javier.barbadillo@gmail.com>
2018-10-28 17:43:36 +0100

    Add client subscriber

---
+++ reactivemqtt/subscriber_client.py
@@ -0,0 +1,20 @@
+import paho.mqtt.client as mqtt
+
+class MqttClient():
+    def __init__(self):
+        client = mqtt.Client()
+        client.on_connect = self.on_connect
+        client.on_message = self.on_message
+        self.client = client
+        client.connect("iot.eclipse.org", 1883, 60)
+        client.loop_start()
+        return client
+
+    def on_connect(self, client, userdat, flags, rc):
+        print("Connected with result code " + str(rc))
+        self.client.subscribe("position",qos=0)
+        self.client.subscribe("objects", qos=0)
+
+    def on_message(self, client, userdata, msg):
+        print(msg.topic + " " + str(msg.payload))
+
