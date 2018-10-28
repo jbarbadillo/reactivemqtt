f355f09fbd745a0cde78c0da41bf28b167372e93
jbarbadillo <javier.barbadillo@gmail.com>
2018-10-28 22:02:40 +0100

    Wait 1 second to stop event

--- reactivemqtt/app.py
+++ reactivemqtt/app.py
@@ -22,9 +22,10 @@ def main():
     data.client.publish("data/object", payload="[1, car]", qos=2)
     data.client.publish("data/position", payload="[2, (0,0,2)", qos=2)
     data.client.publish("data/object", payload="[2, person]", qos=2)
+    sleep(1)
     data.client.publish("event/status", payload="stop_tracking", qos=2)
 
-    sleep(2)
+    sleep(1)
 
 if __name__ == '__main__':
     main()
