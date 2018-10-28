fdca6ffc216587bce7b1f0e8c70f4a4dd3bab744
jbarbadillo <javier.barbadillo@gmail.com>
2018-10-28 22:01:06 +0100

    Add function for processing data

--- reactivemqtt/generic_observer.py
+++ reactivemqtt/generic_observer.py
@@ -4,5 +4,7 @@ class Subscriber:
     def __init__(self, sources):
         Observable.from_(sources) \
             .merge_all() \
-            .subscribe(lambda s: print("{}: {}".format(__name__, s)))
+            .subscribe(self._process_data)
 
+    def _process_data(self, data):
+        print("{}: {}".format(__name__, data))
