071ba56e51fabe76892ad5547d3c2dcfbe9264f3
jbarbadillo <javier.barbadillo@gmail.com>
2018-10-28 21:45:19 +0100

    Add qos 2 for events

--- reactivemqtt/app.py
+++ reactivemqtt/app.py
@@ -13,15 +13,16 @@ def main():
     events = Observable.create(EventClient).share()
     events.subscribe(lambda s: print(s))
 
+    data.client.publish("event/status", payload="start_tracking", qos=2)
     data.client.publish("data/position",payload="[1, (2,2,34)", qos=2)
     data.client.publish("data/object",payload="[1, car]", qos=2)
     data.client.publish("data/position", payload="[1, (2,2,37)", qos=2)
     data.client.publish("data/object", payload="[1, car]", qos=2)
     data.client.publish("data/position", payload="[2, (0,0,2)", qos=2)
     data.client.publish("data/object", payload="[2, person]", qos=2)
+    data.client.publish("event/status", payload="stop_tracking", qos=2)
+
     sleep(2)
-
-
 
 if __name__ == '__main__':
     main()
