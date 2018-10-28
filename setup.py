252776034ffc640f5aa032fdabc10440bd556c78
jbarbadillo <javier.barbadillo@gmail.com>
2018-10-28 16:54:24 +0100

    Add setup

---
+++ setup.py
@@ -0,0 +1,20 @@
+from setuptools import setup
+setup(
+    name = 'reactivemqtt',
+    version='1.0',
+    packages = ['reactivemqtt'],
+    license = 'MIT',
+    description = 'Examples for reactive mqtt',
+    author = "Javier Barbadillo",
+    author_email = "javier.barbadillo@gmail.com",
+    url = 'https://github.com/jbarbadillo/reactivemqtt',
+    download_url = 'https://github.com/jbarbadillo/reactivemqtt/archive/1.0.tar.gz',
+    keywords = ['rx', 'mqtt'],
+    classifiers = ['Programming Language :: Python',
+                   'Programming Language :: Python :: 2.7',
+                   'Programming Language :: Python :: 3.2',
+                   'Programming Language :: Python :: 3.3',
+                   'Programming Language :: Python :: 3.4',
+                   'Programming Language :: Python :: 3.5',
+                   'Programming Language :: Python :: 3.6'],
+)
