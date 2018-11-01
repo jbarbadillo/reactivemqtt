e2048ee68c44991fa24c20b414ef5b2c06055def
jbarbadillo <javier.barbadillo@gmail.com>
2018-11-01 18:54:47 +0100

    Rename data publisher

--- reactivemqtt/app.py
+++ reactivemqtt/app.py
@@ -7,7 +7,7 @@ from reactivemqtt.event_client import EventClient
 from reactivemqtt.data_manager import DataManager
 
 def main():
-    data = MqttClient()
+    data_publisher = MqttClient()
 
     object_positions = Observable.create(ObjectPosClient).share()
     events = Observable.create(EventClient).share()
@@ -15,15 +15,15 @@ def main():
     DataManager([object_positions, events])
 
 
-    data.client.publish("event/status", payload="start_tracking", qos=2)
-    data.client.publish("data/position",payload="[1, (2,2,34)", qos=2)
-    data.client.publish("data/object",payload="[1, car]", qos=2)
-    data.client.publish("data/position", payload="[1, (2,2,37)", qos=2)
-    data.client.publish("data/object", payload="[1, car]", qos=2)
-    data.client.publish("data/position", payload="[2, (0,0,2)", qos=2)
-    data.client.publish("data/object", payload="[2, person]", qos=2)
+    data_publisher.client.publish("event/status", payload="start_tracking", qos=2)
+    data_publisher.client.publish("data/position",payload="[1, (2,2,34)", qos=2)
+    data_publisher.client.publish("data/object",payload="[1, car]", qos=2)
+    data_publisher.client.publish("data/position", payload="[1, (2,2,37)", qos=2)
+    data_publisher.client.publish("data/object", payload="[1, car]", qos=2)
+    data_publisher.client.publish("data/position", payload="[2, (0,0,2)", qos=2)
+    data_publisher.client.publish("data/object", payload="[2, person]", qos=2)
     sleep(1)
-    data.client.publish("event/status", payload="stop_tracking", qos=2)
+    data_publisher.client.publish("event/status", payload="stop_tracking", qos=2)
 
     sleep(1)
 
