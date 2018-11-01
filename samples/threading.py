b56217690f21619559bb773c790f2ccba2ce0efa
jbarbadillo <javier.barbadillo@gmail.com>
2018-11-01 16:13:17 +0100

    Add sample for threading

---
+++ samples/threading.py
@@ -0,0 +1,8 @@
+from rx import Observable
+from rx.concurrency import ThreadPoolScheduler
+from threading import current_thread
+import multiprocessing, time, random
+
+
+
+input("Press key to quit\nq")
