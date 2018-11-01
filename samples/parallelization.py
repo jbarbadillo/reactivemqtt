c102904a0b1b554ffacd007780530394771f310c
jbarbadillo <javier.barbadillo@gmail.com>
2018-11-01 17:00:44 +0100

    Break code into more lines

--- samples/parallelization.py
+++ samples/parallelization.py
@@ -13,8 +13,11 @@ pool_scheduler = ThreadPoolScheduler(optimal_threads)
 print("using {0} thread pool".format(optimal_threads))
 # task 1, process multiple emissions at a time
 Observable.from_(["one", "two", "three", "four", "five", "six", "seven", "eight", "nine", "ten"]) \
-    .flat_map(lambda s: Observable.just(s).subscribe_on(pool_scheduler).map(lambda s: simulate_calculation(s))) \
-    .subscribe(on_next=lambda s: print("thread {0}, value {1}".format(current_thread().name, s)),
+    .flat_map(lambda s:
+              Observable.just(s) \
+              .subscribe_on(pool_scheduler) \
+              .map(lambda s: simulate_calculation(s))
+    ).subscribe(on_next=lambda s: print("thread {0}, value {1}".format(current_thread().name, s)),
                on_completed=lambda: print("finished task 1"))
 
 input("Press key to quit\n")
