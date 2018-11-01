ff5538646151b6abe5d7a23c2ecca2da0fcba1d3
jbarbadillo <javier.barbadillo@gmail.com>
2018-11-01 16:23:37 +0100

    Execute 2 tasks on main thread

--- samples/threading.py
+++ samples/threading.py
@@ -3,6 +3,17 @@ from rx.concurrency import ThreadPoolScheduler
 from threading import current_thread
 import multiprocessing, time, random
 
+def simulate_calculation(value):
+    time.sleep(random.randint(5, 20)*0.1)
+    return value
 
+# thread 1
+Observable.from_(["one", "two", "three", "four", "five"]) \
+    .map(lambda s: simulate_calculation(s)) \
+    .subscribe(on_next=lambda s: print("thread {0}, value {1}".format(current_thread().name, s)))
+# thread 2
+Observable.range(1,5) \
+    .map(lambda s: simulate_calculation(s)) \
+    .subscribe(on_next=lambda s: print("thread {0}, value {1}".format(current_thread().name, s)))
 
 input("Press key to quit\nq")
