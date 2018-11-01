95cf94bbf403f30d6c5283ba274be53382d57fb2
jbarbadillo <javier.barbadillo@gmail.com>
2018-11-01 19:20:10 +0100

    Zip position and objects

--- reactivemqtt/data_receiver.py
+++ reactivemqtt/data_receiver.py
@@ -6,20 +6,21 @@ class DataReceiver:
             .merge_all() \
             .filter(lambda s: s[0] == "event/status" ) \
             .subscribe(self._on_event)
-        Observable.from_(sources) \
+        object_source = Observable.from_(sources) \
             .merge_all() \
             .filter(lambda s: s[0] == "data/object") \
-            .subscribe(self._on_object)
-        Observable.from_(sources) \
+            .map(lambda s: s[1])
+        position_source = Observable.from_(sources) \
             .merge_all() \
             .filter(lambda s: s[0] == "data/position") \
+            .map(lambda s: s[1])
+
+        Observable.zip_array(object_source, position_source) \
             .subscribe(self._on_position)
 
     def _on_event(self, data):
         print("event: {}".format( data[1]))
 
-    def _on_object(self, data):
-        print("object: {}".format( data[1]))
 
     def _on_position(self, data):
-        print("position: {}".format( data[1]))
+        print("position: {}".format( data))
