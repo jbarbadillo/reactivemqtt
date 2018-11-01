607fb3780e22369220dd41678d37d550b6f93bb6
jbarbadillo <javier.barbadillo@gmail.com>
2018-11-01 16:31:30 +0100

    Create thread pool scheduler

--- samples/no_threading.py
+++ samples/no_threading.py
@@ -1,7 +1,6 @@
 from rx import Observable
-from rx.concurrency import ThreadPoolScheduler
 from threading import current_thread
-import multiprocessing, time, random
+import time, random
 
 def simulate_calculation(value):
     time.sleep(random.randint(5, 20)*0.1)
